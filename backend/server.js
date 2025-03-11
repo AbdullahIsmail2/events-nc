b83cd44d4c3b41ff9d26e7cc33026d77f6bc9f6c
Abdullah <abdullahismail1.work@gmail.com>
2025-03-11 15:56:33 +0000

    removing some code related to cors

--- backend/server.js
+++ backend/server.js
@@ -20,33 +20,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "https://events-nc-9rcw.vercel.app",
-        process.env.FRONTEND_URL,
-      ];
-
-      // Allow requests with no origin (e.g., mobile apps or Postman)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 
 
 app.use("/api/events", eventRoutes);
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
-});
-
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
